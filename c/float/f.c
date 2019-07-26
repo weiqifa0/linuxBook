e590a35a72def5a8b8db596265f6db9a2932254c
weiqifa <329410527@qq.com>
2019-07-26 07:28:41 -0700

    add float dir
    	new file:   c/float/a.out
    	new file:   c/float/f.c

---
+++ c/float/f.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+void main(void)
+{
+        float k = 0.5939;
+        int a = -2852;
+        int b = -2936;
+
+        float result = 0.0;
+
+        printf("%f\n",(float)(abs(b - a)/k));
+}
+
